2a54f0f877e26cfb804258ca10a8a55a90216023
Ridwan Khan <rkhan147@myseneca.ca>
2024-01-21 20:36:05 -0500

    fixing glitches

--- WS01/DIY/file.cpp
+++ WS01/DIY/file.cpp
@@ -1,3 +1,16 @@
+/*
+
+Name: Ridwan Khan
+Email: rkhan147@myseneca.ca
+ID: 162409213
+Date: January 21, 2024
+
+I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and
+assignments.
+
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include "file.h"
 #include "StMark.h"
--- WS01/DIY/main.cpp
+++ WS01/DIY/main.cpp
@@ -1,3 +1,16 @@
+/*
+
+Name: Ridwan Khan
+Email: rkhan147@myseneca.ca
+ID: 162409213
+Date: January 21, 2024
+
+I have done all the coding by myself and only copied the
+code that my professor provided to complete my workshops and
+assignments.
+
+*/
+
 /***********************************************************************
 // Workshop 1 # p2: tester program
 //
